60f5e6039ad737d2fc5a929e936affa157097d84
Копылов Александр Николаевич <gr492_kan@ttit.local>
2022-04-29 17:09:26 +0700

    editAddDriver

--- addDriver.php
+++ addDriver.php
@@ -6,14 +6,31 @@ if (!empty($_POST['submit']) && $_POST['submit'] == 'Добавить') {
     $lastname = strip_tags($_POST['lastname']);
     $firstname = strip_tags($_POST['firstname']);
     $patronymic = strip_tags($_POST['patronymic']);
-    $query = "INSERT INTO driver (lastname, firstname, patronymic) VALUES ('$lastname', '$firstname', '$patronymic')";
-    $res = mysqli_query($mysqli, $query);
-    if (!$res) die (mysqli_error($mysqli));
-    // Если количество затронутых запростом записей равно 1 (одна запись добавлена)
-    // то выводим сообщение
-    if (mysqli_affected_rows($mysqli) == 1) {
-        echo "<h2>Водитель добавлен</h2>";
+    $querySelect = "SELECT * from driver WHERE firstname='$firstname' and lastname='$lastname' and patronymic='$patronymic'";
+    $result = mysqli_query($mysqli, $querySelect);
+    $row = mysqli_fetch_array($result);
+    if (empty($lastname) || empty($firstname) || empty($patronymic))
+    {
+        echo "<h1>Заполните все поня!</h1>";
     }
+    else if (!(preg_match('/[а-яёА-ЯЁ]+/u', $firstname) || preg_match('/[а-яёА-ЯЁ]+/u', $lastname) || preg_match('/[а-яёА-ЯЁ]+/u', $patronymic))) {
+        echo "<h1>Поля ФИО должны содержать только русские символы!</h1>";
+    } else if (!((strlen($firstname) < 20 and strlen($firstname) >= 2) or (strlen($lastname) < 20 and strlen($lastname) >= 2) or (strlen($patronymic) < 20 and strlen($patronymic) >= 2))) {
+        echo "<h1>Поля ФИО должны содержать от 2 до 20 символов!</h1>";
+    } else if (!empty($row)) {
+        echo "<h1>Этот водитель уже есть в базе данных!</h1>";
+    }
+    else{
+        $query = "INSERT INTO driver (lastname, firstname, patronymic) VALUES ('$lastname', '$firstname', '$patronymic')";
+        $res = mysqli_query($mysqli, $query);
+        if (!$res) die (mysqli_error($mysqli));
+        // Если количество затронутых запростом записей равно 1 (одна запись добавлена)
+        // то выводим сообщение
+        if (mysqli_affected_rows($mysqli) == 1) {
+            echo "<h2>Водитель добавлен</h2>";
+        }
+    }
+
 }
 ?>
 
@@ -21,15 +38,15 @@ if (!empty($_POST['submit']) && $_POST['submit'] == 'Добавить') {
     <form action="" method="post" class="form">
         <div class="mb-3">
             <label class="form-label">Имя</label>
-            <input type="text" name="firstname" class="form-control" required>
+            <input type="text" name="firstname" class="form-control">
         </div>
         <div class="mb-3">
             <label class="form-label">Фамилия</label>
-            <input type="text" name="lastname" class="form-control" required>
+            <input type="text" name="lastname" class="form-control">
         </div>
         <div class="mb-3">
             <label class="form-label">Отчество</label>
-            <input type="text" name="patronymic" class="form-control" required>
+            <input type="text" name="patronymic" class="form-control">
         </div>
 
         <input type="submit" name="submit" class="btn btn-primary" name="submit" value="Добавить">
--- login.php
+++ login.php
@@ -5,25 +5,19 @@ if (!empty($_POST['submit']) && $_POST['submit'] == 'Войти') {
     $login = strip_tags($_POST['login']);
     $password = strip_tags($_POST['password']);
     $password1 = strip_tags($_POST['password1']);
-    if ($password!=$password1)
-    {
-        echo "<h1>Пароли не сопадают.</h1>";
-    }
-    else
-    {
-        $query = "SELECT * from users WHERE login='$login' AND password='$password'";
-        $res = mysqli_query($mysqli, $query);
-        $user = mysqli_fetch_assoc($res);
-        if (!empty($user)) {
-            $_SESSION['auth'] = true;
-            $_SESSION['login'] = $login;
-            header("Location: http://localhost/7labIZ/index.php");
-            exit();
+    $query = "SELECT * from users WHERE login='$login' AND password='$password'";
+    $res = mysqli_query($mysqli, $query);
+    $user = mysqli_fetch_assoc($res);
+    if (!empty($user)) {
+        $_SESSION['auth'] = true;
+        $_SESSION['login'] = $login;
+        header("Location: http://localhost/7labIZ/index.php");
+        exit();
 
-        } else {
-            echo "<h3>Неправильный логин или пароль</h3>";
-        }
+    } else {
+        echo "<h3>Неправильный логин или пароль</h3>";
     }
+
 }
 
 ?>
@@ -35,10 +29,6 @@ if (!empty($_POST['submit']) && $_POST['submit'] == 'Войти') {
     <div class="mb-3">
         <label class="form-label">Пароль</label>
         <input type="password" name="password" required class="form-control">
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Повторите пароль</label>
-        <input type="password" name="password1" required class="form-control">
     </div>
     <input type="submit" name="submit" class="btn btn-primary" name="submit" value="Войти">
 </form>
